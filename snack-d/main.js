d96b0251ee6d61f17cf84d42a9a7d46d3f9c2375
rudyhuacasi <alexisitaliano16@gmail.com>
2024-05-10 18:46:01 +0200

    {UPD} added javascript (snack d)

---
+++ snack-d/main.js
@@ -0,0 +1,15 @@
+`use strict`
+
+// definisco un variabile per un array(arrayA)
+const arrayA = [0, 1, 2, `elemento3`, 4, 5];
+
+// definisco un variabile per una function rimuoviDallaCoda(arrayA);
+const arrayB = rimuoviDallaCoda(arrayA)
+
+// stampo il arrayB con tutti gli elementi dell'arrayA tranne l'ultimo
+console.log(arrayB);
+
+// impostare una function 
+function rimuoviDallaCoda(array) {
+    
+    return array.slice(0, array.length - 1);}
